8731fa55eb0485d154e816285f7b4f438616c543
gaoyichao <gaoyichao2013@126.com>
2020-08-10 17:05:26 +0800

    echo server poll

---
+++ include/XiaoTuNetBox/TcpServer.h
@@ -0,0 +1,8 @@
+#ifndef XTNB_TCPSERVER_H
+#define XTNB_TCPSERVER_H
+
+
+
+
+#endif
+
--- test/t_echo_server_0.cpp
+++ test/t_echo_server_0.cpp
@@ -4,34 +4,67 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <poll.h>
 
 #include <iostream>
 
 int main() {
     xiaotu::net::IPv4 serverIp(65530);
     xiaotu::net::Socket sock(AF_INET, SOCK_STREAM, 0);
+    sock.SetReuseAddr(true);
+    sock.SetKeepAlive(true);
 
+    int max_conn = 3;
     sock.BindOrDie(serverIp);
-    sock.ListenOrDie(10);
+    sock.ListenOrDie(2);
 
+    struct pollfd pollFds[max_conn];
+    pollFds[0].fd = sock.GetFd();
+    pollFds[0].events = POLLRDNORM;
+    for (int i = 1; i < max_conn; i++)
+        pollFds[i].fd = -1;
+
+    xiaotu::net::IPv4 connections[9];
+    char buf[max_conn-1][1024];
+
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~" << std::endl;
     while (1) {
-        xiaotu::net::IPv4 client;
-        int conn_fd = sock.Accept(&client);
+        int nready = poll(pollFds, max_conn, 100000);
+        std::cout << "nready = " << nready << std::endl;
 
-        if (-1 == conn_fd) {
-            perror("连接失败");
-            exit(1);
-        } else {
-            char buf[1024];
-            while (1) {
-                int nread = read(conn_fd, buf, 1024);
-                if (nread <= 0)
+        if (POLLRDNORM & pollFds[0].revents) {
+            int idx = 1;
+            for (; idx < max_conn; idx++) {
+                if (pollFds[idx].fd < 0)
                     break;
-                std::cout << "nread = " << nread << std::endl;
-                send(conn_fd, buf, nread, 0);
             }
 
-            close(conn_fd);
+            std::cout << "idx = " << idx << std::endl;
+            int conn_fd = sock.Accept(&connections[idx-1]);
+            if (max_conn == idx) {
+                std::cout << "连接太多了" << std::endl;
+                close(conn_fd);
+            } else {
+                pollFds[idx].fd = conn_fd;
+                pollFds[idx].events = POLLRDNORM;
+            }
+        }
+
+        for (int i = 1; i < max_conn; i++) {
+            if (pollFds[i].fd < 0)
+                continue;
+
+            int conn_fd = pollFds[i].fd;
+            if (pollFds[i].revents & (POLLRDNORM | POLLERR)) {
+                int nread = read(conn_fd, buf[i-1], 1024);
+                if (nread <= 0) {
+                    std::cout << "close conn_fd = " << conn_fd << std::endl;
+                    close(conn_fd);
+                    pollFds[i].fd = -1;
+                } else {
+                    send(conn_fd, buf[i-1], nread, 0);
+                }
+            }
         }
     }
 }
